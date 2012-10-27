bce829d13f5e2236a4422542d1923bb29cf37f4b
Danno Ferrin <danno.ferrin@shemnon.com>
2012-10-26 22:31:12 -0600

    Add FXML Login Demo

---
+++ samples/FXML-LoginDemo/src/main/java/demo/Main.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2012, Oracle and/or its affiliates. All rights reserved.
+ */
+package demo;
+
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import demo.model.User;
+import demo.security.Authenticator;
+
+/**
+ * Main Application. This class handles navigation and user session.
+ */
+public class Main extends Application {
+
+    private Stage stage;
+    private User loggedUser;
+    private final double MINIMUM_WINDOW_WIDTH = 390.0;
+    private final double MINIMUM_WINDOW_HEIGHT = 500.0;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(Main.class, (java.lang.String[])null);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            stage = primaryStage;
+            stage.setTitle("FXML Login Sample");
+            stage.setMinWidth(MINIMUM_WINDOW_WIDTH);
+            stage.setMinHeight(MINIMUM_WINDOW_HEIGHT);
+            gotoLogin();
+            primaryStage.show();
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public User getLoggedUser() {
+        return loggedUser;
+    }
+        
+    public boolean userLogging(String userId, String password){
+        if (Authenticator.validate(userId, password)) {
+            loggedUser = User.of(userId);
+            gotoProfile();
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    void userLogout(){
+        loggedUser = null;
+        gotoLogin();
+    }
+    
+    private void gotoProfile() {
+        try {
+            ProfileController profile = (ProfileController) replaceSceneContent("Profile.fxml");
+            profile.setApp(this);
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void gotoLogin() {
+        try {
+            LoginController login = (LoginController) replaceSceneContent("Login.fxml");
+            login.setApp(this);
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private Initializable replaceSceneContent(String fxml) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        InputStream in = Main.class.getResourceAsStream(fxml);
+        loader.setBuilderFactory(new JavaFXBuilderFactory());
+        loader.setLocation(Main.class.getResource(fxml));
+        AnchorPane page;
+        try {
+            page = (AnchorPane) loader.load(in);
+        } finally {
+            in.close();
+        } 
+        Scene scene = new Scene(page, 800, 600);
+        stage.setScene(scene);
+        stage.sizeToScene();
+        return (Initializable) loader.getController();
+    }
+}
