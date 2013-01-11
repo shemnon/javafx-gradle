1eb177ef799510a4bec9b38a76df037077cf5486
Danno Ferrin <danno.ferrin@shemnon.com>
2013-01-11 16:22:22 -0700

    Add Slide to unlock control via extrenal library Change hello to "Prepare for takeoff" TIL "Prepare for takeoff" is one word in German.

--- samples/FullyExpressed/src/main/java/com/bitbucket/shemnon/jfx/sample/fullyexpressed/TheMain.java
+++ samples/FullyExpressed/src/main/java/com/bitbucket/shemnon/jfx/sample/fullyexpressed/TheMain.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import jfxtras.labs.scene.control.SlideLockBuilder;
 
 import java.util.ResourceBundle;
 
@@ -15,6 +17,10 @@ public class TheMain extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setResources(ResourceBundle.getBundle("com.bitbucket.shemnon.jfx.sample.fullyexpressed.TheResources"));
         StackPane pane = (StackPane) fxmlLoader.load(this.getClass().getResource("theScene.fxml").openStream());
+
+        ((VBox) fxmlLoader.getNamespace().get("VBox")).getChildren().add(
+            SlideLockBuilder.create().build()
+        );
 
         Scene scene = new Scene(pane);
         scene.getStylesheets().setAll(
